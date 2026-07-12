1f97dd1c1eec5beeeeb31a4b657dafda6519daf5
Tongas02 <113727277+Tongas02@users.noreply.github.com>
2026-07-12 17:52:24 -0300

    las url, data y mis publicaciones corregidas

--- apps/propiedades/data.py
+++ apps/propiedades/data.py
@@ -1,14 +1,59 @@
 import json
 from pathlib import Path
 
+
 _BASE = Path(__file__).parent
 _JSON_PATH = _BASE / "ciudades_paraguay.json"
+
 
 try:
     with open(_JSON_PATH, "r", encoding="utf-8") as f:
         DEPARTAMENTOS_CIUDADES = json.load(f)
+
 except Exception:
-    DEPARTAMENTOS_CIUDADES = {}
+    DEPARTAMENTOS_CIUDADES = {
+        "Itapúa": [
+            "Encarnación",
+            "Hohenau",
+            "Obligado",
+            "Bella Vista",
+            "Pirapó",
+            "Capitán Meza",
+            "Fram",
+            "Carmen del Paraná",
+            "Coronel Bogado",
+            "San Juan del Paraná",
+            "Cambyretá",
+            "Natalio",
+            "Edelira",
+            "Tomás Romero Pereira",
+            "Mayor Otaño",
+        ],
+        "Alto Paraná": [
+            "Ciudad del Este",
+            "Hernandarias",
+            "Presidente Franco",
+            "Minga Guazú",
+            "Santa Rita",
+            "Naranjal",
+            "Juan León Mallorquín",
+        ],
+        "Central": [
+            "San Lorenzo",
+            "Luque",
+            "Capiatá",
+            "Fernando de la Mora",
+            "Lambaré",
+            "Mariano Roque Alonso",
+            "Limpio",
+            "Ñemby",
+            "Villa Elisa",
+            "Itauguá",
+        ],
+        "Asunción": [
+            "Asunción",
+        ],
+    }
 
 
 def get_ciudades_for_departamento(departamento):
