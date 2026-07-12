6c4e68bb4b3ecdc92f5f63e45a6c615d69f25110
JorgeDoming <jd0570328@gmail.com>
2026-07-12 18:03:27 -0300

    ubicacion y detalles de la propiedad

---
+++ activate.bat
@@ -0,0 +1,8 @@
+* [32mmain[m
+  [31mremotes/origin/HEAD[m -> origin/main
+  [31mremotes/origin/Inicio[m
+  [31mremotes/origin/feature/gaston-ciudades-publicaciones[m
+  [31mremotes/origin/feature/jorge-ubicacion-detalle[m
+  [31mremotes/origin/feature/pruebas-unitarias[m
+  [31mremotes/origin/login[m
+  [31mremotes/origin/main[m
